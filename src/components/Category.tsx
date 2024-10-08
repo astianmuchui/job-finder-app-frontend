7e5350561d80d7dc981906ea30ab0a05c6ad3a4e
Kinotijoan <kinotijoan@gmail.com>
2024-10-08 22:45:26 +0300

    feat: Introduce Category component with CompanyMarquee
    Creates a new Category component that currently features the CompanyMarquee. This serves as a foundational step for further development of category-specific functionalities.

---
+++ src/components/Category.tsx
@@ -0,0 +1,11 @@
+import CompanyMarquee from "@/components/CompanyMarquee";
+
+const Category = () => {
+    return (
+        <div className={"bg-purple-100 text-black  h-screen"}>
+          <CompanyMarquee/>
+        </div>
+    );
+}
+
+export default Category;
