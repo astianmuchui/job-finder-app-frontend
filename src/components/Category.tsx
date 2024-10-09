b68506dcb06130b40f70f9279822685185bca434
Kinotijoan <kinotijoan@gmail.com>
2024-10-09 16:43:55 +0300

    feat: Implement category card pagination
    Introduces pagination to the category browsing section, enhancing user experience by allowing users to navigate through categories efficiently, especially on smaller screens where content might overflow. This makes browsing and finding desired categories more manageable.

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,9 +1,60 @@
 import CompanyMarquee from "@/components/CompanyMarquee";
+import {ArrowBigLeftDash, ArrowBigRightDash} from "lucide-react";
+import CategoryCard from "@/components/CategoryCard";
+import React, {useState} from "react";
+
+// Total number of cards (adjust this to your data)
+const TOTAL_CARDS = 10;
 
 const Category = () => {
+
+    const [currentPage, setCurrentPage] = useState(0);
+
+
+    const cardsPerPage = window.innerWidth >= 1024 ? 8 : 4;
+
+    const cards = Array.from({ length: TOTAL_CARDS }, (_, i) => <CategoryCard key={i} />);
+
+
+    const totalPages = Math.ceil(cards.length / cardsPerPage);
+
+
+    const currentCards = cards.slice(currentPage * cardsPerPage, (currentPage + 1) * cardsPerPage);
+
+
+    const handleNext = () => {
+        if (currentPage < totalPages - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+
+    const handlePrev = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
     return (
-        <div className={"bg-purple-100 text-black  h-screen"}>
-          <CompanyMarquee/>
+        <div className={"bg-purple-100 text-black h-[100vh] flex flex-col space-y-10"}>
+            <CompanyMarquee/>
+            <div className={" flex flex-col items-center space-y-4 mt-4"}>
+                <h1 className={"font-bold text-xl text-center lg:text-5xl lg:text-center"}>Browse By Categories</h1>
+                <p className={"text-md lg:text-2xl font-light"}>Discover the perfect job tailored just for you</p>
+            </div>
+            <div className={"flex flex-row justify-center items-center"}>
+                <div className={" bg-purple-800/70  text-white/70 rounded-full m-2 p-1"} onClick={() => handlePrev()}>
+
+                    <ArrowBigLeftDash size={30}/>
+                </div>
+                <div className={"w-[80%] bg-purple-100 h-[60vh] lg:h-[40vh] flex flex-col space-y-4 lg:flex lg:flex-row lg:flex-wrap justify-center items-center p-2 lg:space-y-0 lg:space-x-4"}>
+                    {currentCards}
+                </div>
+                <div className={"bg-purple-800/70 text-white/70  rounded-full m-2 p-1"} onClick={() => handleNext()}>
+
+                    <ArrowBigRightDash size={30}/>
+                </div>
+            </div>
         </div>
     );
 }
