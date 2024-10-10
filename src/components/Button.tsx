6a8d9f5c0bd287ceaddf25c79cdbbd0eced3b359
astianmuchui <astianmuchui@gmail.com>
2024-10-10 11:22:11 +0300

    Installed prisma ORM and pulled db schema into prisma schema

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+
+
+const Button = () => {
+  return (
+    <>
+    <button className="px-4 py-2 bg-gradient-to-tr from-purple-800/70 via-purple-900/50 to-purple-950">
+    Get started
+    </button>
+    </>
+  )
+}
+
+export default Button
