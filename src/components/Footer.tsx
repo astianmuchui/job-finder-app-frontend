69f846d7ad0b7836aa1b2e4e876e7a73c4fe2555
Kinotijoan <kinotijoan@gmail.com>
2024-10-10 11:02:15 +0300

    feat: Introduce Footer component
    Add a new Footer component to enhance the visual structure and provide space for potential future content.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+     <div className={"bg-purple-800 h-[20rem] flex flex-col justify-center items-center"}>
+         </div>
+
+    );
+
+}
+
+export default Footer;
