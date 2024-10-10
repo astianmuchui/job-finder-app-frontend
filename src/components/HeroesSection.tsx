a0c563171e9e7a0f5baac683579c417fe053129e
Kinotijoan <kinotijoan@gmail.com>
2024-10-10 11:02:26 +0300

    feat: Enhance hero section design and navigation
    Updates the hero section to improve visual appeal and user experience.
    
    This includes replacing the placeholder navbar with a direct link to the homepage and refining the search bar's appearance for a cleaner look and feel. The "Get Started" button is also visually enhanced to better attract user attention.

--- src/components/HeroesSection.tsx
+++ src/components/HeroesSection.tsx
@@ -1,14 +1,14 @@
 'use client'
 import React from "react";
-import NavBar from "@/components/NavBar";
 import {name} from "@/lib/Constants";
 import {Search} from "lucide-react";
+import Link from "next/link";
 
 const HeroesSection = () => {
     return (
         <>
-            <div className={"overflow-hidden relative w-full h-[90vh]"}>
-                <NavBar/>
+            <div id={"home"} className={"overflow-hidden relative w-full h-[90vh]"}>
+
                 <div className={"text-white bg-purple-100 lg:pb-0  lg:mt-16 w-full flex  h-[100%]"}>
                     <div
                         className={"hidden lg:block lg:absolute lg:bg-purple-800/70 lg:backdrop:filter lg:backdrop:backdrop-blur-md lg:w-[50%] lg:left-[1250px] lg:h-[100%] lg:rounded-bl-[15rem] rotate-[40deg] "}>
@@ -24,12 +24,16 @@ const HeroesSection = () => {
                             <div className={"flex flex-row items-center pl-2"}>
 
                                 <Search size={20} className={"text-black/40"}/>
-                                <input type="text" placeholder={"Search for jobs"}
-                                       className={"p-2 rounded-lg border-0 active:border-0 "}/>
+                                <input type="text" placeholder="Search for jobs"
+                                       className="p-2 rounded-lg border-0 focus:outline-none focus:ring-0"/>
+
                             </div>
+                            <Link href="/">
                             <button
-                                className={"bg-purple-800/70 p-2 text-lg rounded-xl text-white hover:bg-transparent hover:border-2 hover:outline-none hover:border-purple-800/70 hover:text-black lg:w-[30%] "}>Search
-                            </button>
+                                    className="w-max p-3 bg-gradient-to-tr from-purple-800/70 via-purple-900/70 to-purple-950/70 text-white rounded-lg hover:bg-gradient-to-br">
+                                    Get Started
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
