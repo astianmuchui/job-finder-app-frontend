28775d1997af59b364c80ca3724647c77d57054f
Kinotijoan <kinotijoan@gmail.com>
2024-10-12 19:04:02 +0300

    feat: Implement robust form validation and handling
    Integrated form validation and submission logic using Zod and React Hook Form for login, signup, and password reset functionalities. This enhances user experience by providing real-time feedback, ensuring data integrity, and streamlining the authentication process.
    
    feat: Enhance authentication with form validation
    
    Implemented robust form validation and submission handling using Zod and React Hook Form for login, signup, and password reset components.
    
    This provides real-time feedback to users, ensuring data integrity and a smoother authentication experience.

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -2,8 +2,42 @@
 
 import {Eye, EyeOff} from "lucide-react";
 import {useState} from "react";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {SubmitHandler, useForm} from "react-hook-form";
+import axios from "axios";
+import {useParams, useRouter} from "next/navigation";
+
+const ResetSchema = z.object({
+    password: z.string({required_error: "Password is required"}).min(8, {message: "Password must be at least 8 characters"}).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {message: "Password must contain at least one uppercase letter, one lowercase letter and one number"}),
+    confirmPassword: z.string({ required_error: "Password confirmation is required" }),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match. Please make sure both passwords are identical",
+    path: ["confirmPassword"],
+});
+
+type ResetSchema = z.infer<typeof ResetSchema>
 
 const ResetPassword = () => {
+    const router = useRouter();
+    const {verificationCode} = useParams();
+    const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm<ResetSchema>({
+        resolver: zodResolver(ResetSchema)
+    });
+
+    const onSubmit: SubmitHandler<ResetSchema> = (data) => {
+        const {confirmPassword, ...rest} = data;
+        axios.post(`http://localhost:3000/api/resetPassword/${verificationCode}`, rest).then((response) => {
+            console.log(response.data);
+            if (response.status === 200) {
+                router.push("/login");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+
+    };
+
     const [passwordVisible, setPasswordVisible] = useState(false);
 
     const togglePasswordVisibility = () => {
@@ -17,27 +51,32 @@ const ResetPassword = () => {
 
             </div>
             <div className={"space-y-6 w-[90%]"}>
-                <form className={"flex flex-col space-y-4 items-center text-black"}>
+                <form onSubmit={handleSubmit(onSubmit)} className={"flex flex-col space-y-4 items-center text-black"}>
 
                     <div
                         className={"flex flex-row justify-between items-center  w-[80%] border-2 border-purple-800/40 rounded-3xl focus:outline-none focus:ring-0"}>
-                        <input type={passwordVisible ? "password" : "text"} placeholder="New Password"
+                        <input disabled={isSubmitting} {...register("password")}
+                               type={passwordVisible ? "password" : "text"} placeholder="New Password"
                                className={"rounded-bl-3xl rounded-tl-3xl border-0 focus:outline-none focus:ring-0 w-[100%] "}/>
                         {passwordVisible ?
                             <Eye size={25} className={"text-black/50 mr-2"} onClick={togglePasswordVisibility}/> :
                             <EyeOff size={23} className={"text-black/50 mr-2"} onClick={togglePasswordVisibility}/>}
 
                     </div>
+                    {errors.password && <p className={"text-red-500 self-start mx-14"}>{errors.password.message}</p>}
                     <div
                         className={"flex flex-row justify-between items-center  w-[80%] border-2 border-purple-800/40 rounded-3xl focus:outline-none focus:ring-0"}>
-                        <input type={passwordVisible ? "password" : "text"} placeholder="Confirm Password"
+                        <input disabled={isSubmitting} {...register("confirmPassword")}
+                               type={passwordVisible ? "password" : "text"} placeholder="Confirm Password"
                                className={"w-[100%] rounded-bl-3xl rounded-tl-3xl border-0 focus:outline-none focus:ring-0"}/>
                         {passwordVisible ?
                             <Eye size={25} className={"text-black/50 mr-2 ]"} onClick={togglePasswordVisibility}/> :
                             <EyeOff size={23} className={"text-black/50 mr-2 ]"} onClick={togglePasswordVisibility}/>}
                     </div>
+                    {errors.confirmPassword &&
+                        <p className={"text-red-500 self-start mx-14"}>{errors.confirmPassword.message}</p>}
                     <div className={"flex w-[80%] space-x-4"}>
-                        <button type="submit"
+                        <button disabled={isSubmitting} type="submit"
                                 className={"w-[80%] mx-auto p-2 bg-gradient-to-tr from-purple-800/70 via-purple-900/70 to-purple-950/70 text-white rounded-3xl hover:bg-gradient-to-br"}>
                             Reset Password
                         </button>
@@ -49,5 +88,7 @@ const ResetPassword = () => {
     );
 
 }
+
+
 
 export default ResetPassword;
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -2,8 +2,48 @@
 
 import {useState} from "react";
 import {Eye, EyeOff} from "lucide-react";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+const SignupSchema = z.object({
+    username: z.string({ required_error: "Name is required" }).min(1, { message: "Username is required. Please enter a username" }), // Ensure username is required
+    email: z.string({ required_error: "Email is required" }).email({ message: "Please enter a valid email address" }),
+    password: z.string({ required_error: "Please fill out this field" })
+        .min(8, { message: "Password must be at least 8 characters" })
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, { message: "Password must contain at least one uppercase letter, one lowercase letter, and one number" }),
+    confirmPassword: z.string({ required_error: "Password confirmation is required" }),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match. Please make sure both passwords are identical",
+    path: ["confirmPassword"],
+});
+
+type SignupSchema = z.infer<typeof SignupSchema>
 
 const SignUpForm = () => {
+
+    const {register, handleSubmit , formState: {errors, isSubmitting}} = useForm<SignupSchema>({
+        resolver: zodResolver(SignupSchema)
+    });
+
+    const router = useRouter();
+    const onSubmit: SubmitHandler<SignupSchema> = async (data) => {
+        const { confirmPassword, ...rest } = data; // Exclude confirmPassword from submitted data
+        axios.post(
+            "http://localhost:3000/api/signup",
+            rest
+        ).then((response) => {
+            if(response.status === 200){
+                router.push("/verifyEmail");
+            }
+
+            console.log(response.data);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
 
     const [passwordVisible, setPasswordVisible] = useState(false);
 
@@ -21,35 +61,38 @@ const SignUpForm = () => {
                 </p>
             </div>
             <div className={"space-y-6 w-[90%]"}>
-                <form className={"flex flex-col space-y-4 items-center text-black"}>
+                <form onSubmit={handleSubmit(onSubmit)} className={"flex flex-col space-y-4 items-center text-black"}>
                     <div className={"w-[80%] border-2 border-purple-800/40 rounded-3xl "}>
 
-                    <input type="text" placeholder="Full Name"
+                    <input {...register("username")} type="text" placeholder="Username" disabled={isSubmitting}
                            className={"p-2 w-[100%] border-0 border-purple-800/40 rounded-3xl focus:outline-none focus:ring-0"}/>
                     </div>
+                        {errors.username && <p className={"text-red-500 self-start mx-14"}>{errors.username.message}</p>}
                     <div className={"w-[80%] border-2 border-purple-800/40 rounded-3xl "}>
 
-                    <input type="text" placeholder="Email"
+                    <input {...register("email")} type="text" placeholder="Email" disabled={isSubmitting}
                            className={"p-2 w-[100%] border-0 border-purple-800/40 rounded-3xl focus:outline-none focus:ring-0"}/>
                     </div>
+                        {errors.email && <p className={"text-red-500 self-start mx-14"}>{errors.email.message}</p>}
 
                     <div
                         className={"flex flex-row justify-between items-center  w-[80%] border-2 border-purple-800/40 rounded-3xl focus:outline-none focus:ring-0"}>
-                        <input type={passwordVisible ? "password" : "text"} placeholder="Password"
+                        <input {...register("password")} type={passwordVisible ? "password" : "text"} placeholder="Password" disabled={isSubmitting}
                                className={"rounded-bl-3xl rounded-tl-3xl border-0 focus:outline-none focus:ring-0 w-[100%] "}/>
                         { passwordVisible ? <Eye size={25} className={"text-black/50 mr-2"} onClick={togglePasswordVisibility}/> : <EyeOff size={23} className={"text-black/50 mr-2"} onClick={togglePasswordVisibility}/> }
-
                     </div>
+                        {errors.password && <p className={"text-red-500 self-start mx-14 text-start"}>{errors.password.message}</p>}
                     <div
                         className={"flex flex-row justify-between items-center  w-[80%] border-2 border-purple-800/40 rounded-3xl focus:outline-none focus:ring-0"}>
-                        <input type={passwordVisible ? "password" : "text"} placeholder="Confirm Password"
+                        <input {...register("confirmPassword")} type={passwordVisible ? "password" : "text"} placeholder="Confirm Password" disabled={isSubmitting}
                                className={"w-[100%] rounded-bl-3xl rounded-tl-3xl border-0 focus:outline-none focus:ring-0"}/>
                         { passwordVisible ? <Eye size={25} className={"text-black/50 mr-2 ]"} onClick={togglePasswordVisibility}/> : <EyeOff size={23} className={"text-black/50 mr-2 ]"} onClick={togglePasswordVisibility}/> }
                     </div>
+                        {errors.confirmPassword && <p className={"text-red-500 self-start mx-14"}>{errors.confirmPassword.message}</p>}
 
-                    <button type="submit"
+                    <button type="submit" disabled={isSubmitting}
                             className={"w-[80%] p-2 bg-gradient-to-tr from-purple-800/70 via-purple-900/70 to-purple-950/70 text-white rounded-3xl hover:bg-gradient-to-br"}>
-                        Login
+                        Sign Up
                     </button>
 
                     <div className={"flex flex-row justify-center items-center space-x-2"}>
