f8a5607f7e5a675d6be14fa6a14ac564d5542e39
astianmuchui <astianmuchui@gmail.com>
2024-10-12 11:52:08 +0300

    Refactor layout and modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,10 +21,10 @@ export default function Modal({ children, trigger }: { children: React.ReactNode
     }, [trigger]);
 
     return (
-        <div className="modal fixed  inset-0 w-full h-full  z-50 bg-black/50 flex items-center justify-center" id="modal">
-            <div className="w-[60vw] h-[85vh] bg-white rounded-xl z-50 p-3" id="modal-content">
-                <div className="w-full h-12 p-2">
-                    <span id="closer" className="w-4 ml-[95%] h-4 bg-gradient-to-r from-purple-700 to-purple-900 hover:bg-gradient-to-bl px-3 py-2  cursor-pointer rounded-full"
+        <div className="modal fixed hidden  inset-0 w-full h-full  z-50 bg-black/50 flex items-center justify-center" id="modal">
+            <div className="w-[60vw] h-[90vh] max-h-[95vh] bg-white rounded-xl z-50 px-3 py-1" id="modal-content">
+                <div className="w-full h-12 p-3">
+                    <span id="closer" className="w-4 mt-5 ml-[95%] h-4 bg-gradient-to-r from-purple-700 to-purple-900 hover:bg-gradient-to-bl px-3 py-2  cursor-pointer rounded-full"
                         onClick={() => {
                             const modal = document.querySelector('.modal');
                             if (modal) modal.classList.toggle('hidden');
