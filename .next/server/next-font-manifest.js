a9e4ee847e92f087a6fa7f096ea725a9057bc150
Kinotijoan <kinotijoan@gmail.com>
2024-10-13 15:57:11 +0300

    Stop tracking .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/run/media/nkatha/dossiers/Codes/job-finder-app-frontend/src/app/layout\":[\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
